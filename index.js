527d1317d5d403cfcca39d2a08909f78002e9f3e
narongsak keawmanee <klogic@hotmail.co.th>
2019-12-05 15:40:57 +0700

    connect with mysql successfull.

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("working");
+  })
+  .catch(err => {
+    console.log("not working" + err);
+  });
